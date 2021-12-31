38ed09184fa0b38271ff9f8fa1213bf733252416
Shaarif <shaarifshahid@gmail.com>
2022-01-01 02:44:53 +0500

    interactive resume

---
+++ javascript.js
@@ -0,0 +1,13 @@
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+
+  function jump() {
+    document.getElementById("jump_to_this_location").scrollIntoView({behavior: 'smooth'});
+  }
